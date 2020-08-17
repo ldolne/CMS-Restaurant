d83080ec14a3d6c19592c5dcd7480b6ef71a6480
ldolne <dolnelaetitia@gmail.com>
2020-08-17 23:26:53 +0200

    Add files structure

---
+++ wp-content/themes/CMS-Restaurant/functions.php
@@ -0,0 +1,24 @@
+<? php
+
+
+// enlever Gutenberg
+
+  // for posts
+  add_filter('use_block_editor_for_post', '__return_false', 10);
+  // for post types
+  add_filter('use_block_editor_for_post_type', '__return_false', 10);
+  
+  
+  // Nettoyer dashboard avant remise au client
+  /*
+  
+    function my_wpadmin_sidebar_menu() {
+      remove_menu_page( 'index.php' );  // 'Dashboard'
+      remove_menu_page( 'upload.php' );   // 'Media'
+      remove_menu_page( 'edit-comments.php' )
+      remove_submenu_page('upload.php','media-new.php'); 
+      remove_submenu_page('plugins.php','plugin-install.php'); //plugins
+      remove_submenu_page('plugins.php','plugin-editor.php');
+  }
+  add_action('admin_menu','my_wpadmin_sidebar_menu', 999);
+  */
